fba6bc980d5de2a9b5691cbb861ca6aa5a3b2f96
Steve <anil.stephen66@gmail.com>
2024-11-15 14:00:15 +0530

    Show scheduled posts in single.php

--- lib/class-gtc-theme.php
+++ lib/class-gtc-theme.php
@@ -8,6 +8,7 @@ class GTC_THEME {
   public function __construct() {
 
     add_action( 'wp_enqueue_scripts', array( $this, 'assets' ) );  // LOAD ASSETS
+    add_filter('the_posts', array( $this, 'show_scheduled_posts' ) ); // SHOW SCHEDULED POSTS IN SINGLE.PHP
 
     /* ADD SOW FROM THE THEME */
     add_action('siteorigin_widgets_widget_folders', function( $folders ){
@@ -22,6 +23,16 @@ class GTC_THEME {
     // ENQUEUE STYLES
     wp_enqueue_style('gtc-core-css', GTC_THEME_URI.'/assets/css/main.css', array('sp-core-style'), GTC_THEME_VERSION );
 
+  }
+
+  function show_scheduled_posts( $posts ){
+    global $wp_query, $wpdb;
+
+    if( is_single() && $wp_query->post_count == 0 ){
+      $posts = $wpdb->get_results( $wp_query->request );
+    }
+
+    return $posts;
   }
 
 }
